08aa4d29a6ef3a7c98ea8071dc3f1cdc3bf81c73
xxLestadxx <j.v.gospodinov@gmail.com>
2018-11-12 10:18:26 +0000

    almost finished

--- assignment.py
+++ assignment.py
@@ -42,66 +42,60 @@ def plot_colours(col, perm):
 	axes.axis('off')
 	plt.show()
 
+'''
+This is the Greedy algorithm
+@param setOfColours takes the set of colours
+@param randomColour takes a random number to indicate the first random colour
+'''
 def constructive(setOfColours,randomColour):
+    #initializing of variables and lists
     constructive_order = []
     res = []
     sumOfSolutions = 0
-    colours_copy = setOfColours[0:len(setOfColours)]
-    n = randomColour
     # where n is the first random color and then is the next color
+    n = randomColour
+
+    #creating a copy of the original list, to keep track of used colours
+    colours_copy = setOfColours[0:len(setOfColours)]
+
+    #add the first colour to the list and deletes it from the copy list
     constructive_order.append(setOfColours.index(colours_copy[n]))
-    #print "index ", setOfColours.index(colours_copy[n])
-    #print "first color ", colours_copy[n]
     del colours_copy[n]
 
     while(len(constructive_order)<len(setOfColours)):
         for p in range(len(colours_copy)):
-            #print "setOfColours color", setOfColours[n]
-            #print "colours_copy color ", colours_copy[p]
             result = sqrt((float(setOfColours[n][0]) - float(colours_copy[p][0]))**2 + (float(setOfColours[n][1]) - float(colours_copy[p][1]))**2 + (float(setOfColours[n][2]) - float(colours_copy[p][2]))**2)
             res.append(result)
             best = min(res)
             i = res.index(best)
 
-        #print "best is ", best
-    #    print "res ot ",i, " e to ", res[i]
-    #    print "colors copy ot i e ", colours_copy[i]
-#        print "color e ", colours.index(colours_copy[i])
         sumOfSolutions += best
         n = setOfColours.index(colours_copy[i])
-#        print "n e ", n
-#        print "colors original ot i e " ,colours[i]
+        #resetting the result list so that there would be updated correct values
         res = []
+        #adding the colour with minimum distance and deleting it from the copy list
         constructive_order.append(setOfColours.index(colours_copy[i]))
         del colours_copy[i]
 
     return constructive_order, sumOfSolutions
 
 
-
-def listOfIndexesIntoColours(listOfIndexes):
-    list = []
-    for i in listOfIndexes:
-        list.append(colours[i])
-
-    return list
-
-def swap_random(seq):
-         idx = range(len(seq))
-         i1, i2 = random.sample(idx, 2)
-         seq[i1], seq[i2] = seq[i2], seq[i1]
-
-
+'''
+Inverts the two colours in the list
+taking as parameters the listToSwap, and the two colours
+'''
 def inversion(listToSwap, first, second):
 
-    #listToSwap[idxStart], listToSwap[idxEnd] = listToSwap[idxEnd], listToSwap[idxStart]
     newList = listToSwap[first : second]
     newList.reverse()
 
     return newList
 
 
-
+'''
+Evaluates sum of distances of the provided list of colours
+Returns the total sum of distances
+'''
 def evaluate(sol):
     total = 0
     for i in range(len(sol)-1):
@@ -112,75 +106,105 @@ def evaluate(sol):
 
     return total
 
-
+'''
+A method to create a new random neighbour, by taking the list and then
+inverting randomly two indexes
+'''
 def random_neighbour(s):
         neighbour = deepcopy(s)
         list = [0,0]
         list[0] = random.randint(0,len(neighbour)-1)
         list[1] = random.randint(0,len(neighbour)-1)
-        #this is to make sure that the first is going to be bigger then the second index
+        #sorting is to make sure that the first is going to be bigger then the second index and that everything is going to be alright
         list.sort()
         neighbour[list[0] : list[1] + 1] = inversion(neighbour, list[0], list[1] + 1)
 
         return neighbour
-
+'''
+Simple implementation of the Hill Climbing algorithm
+Takes setOfColours and numberOfIterations
+Returns the best solution, the best solution's sum of distance and the list with all evaluations from the execution
+'''
 def hill_climbing(setOfColours, numberOfIterations):
     #shuffling the list of colours so that i can have a random start
     sol = random.sample(range(len(setOfColours)), len(setOfColours)) # list of colours for testing
     #list that will save all improvements from the eval_neighbour_sol to be then used for graph
     solListEval = []
+    #evaluating the first solution and adding it to the list with solutions
     eval_sol = evaluate(sol)
     solListEval.append(eval_sol)
+
     while (numberOfIterations>0):
+        #taking the first/best solution and inverting 2 indexes to create new neighbour then evaluating it
         neighbour_sol = random_neighbour(sol)
         eval_neighbour_sol = evaluate(neighbour_sol)
+        #if the new solution is better, then the new solution becomes the best one
         if (eval_neighbour_sol < eval_sol):
             #print "eval_neighbour_sol e ", eval_neighbour_sol
             sol = deepcopy(neighbour_sol)
             eval_sol = eval_neighbour_sol
 
         numberOfIterations -=1
+        #at the end appending a value, to keep track for the graph
         solListEval.append(eval_sol)
 
     return sol, eval_sol, solListEval
 
-
+'''
+Multi hill climbing algotrithm, runs the hill climbing for multiple times, takes the best solution at the end
+Takes: tries - the number of iterations of the HC algorithm, setOfColours and hillclimbingIterations
+Returns: best solution, the sum of distances of the best, list with evaluations of all the solutions
+'''
 
 def multi_hc(tries, setOfColours,hillclimbingIterations):
 
-    bestSolIndexes = []
+    bestSolIndecies = []
     bestSolEval = 0
-
-    worstSolIndexes =[]
-    worstSolEval = 0
-
-    values = []
+    valuesMulti = []
 
     for i in range(0, tries):
-        solIndexes,solSum, val = hill_climbing(setOfColours,hillclimbingIterations)
-
-        #values.append(solSum)
-
-        if len(bestSolIndexes) < 1:
-            bestSolIndexes = solIndexes[:]
+        solIndecies,solSum, val = hill_climbing(setOfColours,hillclimbingIterations)
+        valuesMulti.append(solSum)
+        if len(bestSolIndecies) < 1:
+            bestSolIndecies = solIndecies[:]
             bestSolEval = solSum
-
-            worstSolIndexes = solIndexes[:]
-            worstSolEval = solSum
             continue;
-
         if (bestSolEval > solSum):
-            bestSolIndexes = solIndexes[:]
+            bestSolIndecies = solIndecies[:]
             bestSolEval = solSum
 
-        if (worstSolEval < solSum):
-            worstSolIndexes = solIndexes[:]
-            worstSolEval = solSum
+    return bestSolIndecies, bestSolEval, valuesMulti
 
-    return bestSolIndexes, bestSolEval, worstSolIndexes, worstSolEval
+'''
+Multi Simulated annealing algotrithm, runs the SA for multiple times, takes the best solution at the end
+Takes: tries - the number of iterations of the SA algorithm, setOfColours, temperature and simulatedAnnealingIterations
+Returns: best solution, the sum of distances of the best, list with evaluations of all the solutions
+'''
 
+def multi_sa(tries, setOfColours,temperatureAnnealing,simulatedAnnealingIterations):
 
+    bestSolIndecies = []
+    bestSolEval = 0
+    valuesMulti = []
 
+    for i in range(0, tries):
+        annealingIndecies, sumOfEval, valuesAnnealing = annealing(test_colours,temperatureAnnealing, simulatedAnnealingIterations)
+        valuesMulti.append(sumOfEval)
+        if len(bestSolIndecies) < 1:
+            bestSolIndecies = annealingIndecies[:]
+            bestSolEval = sumOfEval
+            continue;
+        if (bestSolEval > sumOfEval):
+            bestSolIndecies = annealingIndecies[:]
+            bestSolEval = sumOfEval
+
+    return bestSolIndecies, bestSolEval, valuesMulti
+
+'''
+Simple implementation of simulated annealing algorithm
+Takes setOfColours, temperature and number of Iterations, for every time the temperature drops
+Returns best solution, the sum distance of the best solution and the list of all solutions
+'''
 def annealing(setOfColours,temperature,numberOfIterations):
     #shuffling the list of colours so that i can have a random start
     sol = random.sample(range(len(setOfColours)), len(setOfColours)) # list of colours for testing
@@ -194,16 +218,18 @@ def annealing(setOfColours,temperature,numberOfIterations):
 
     while (tempmin < temperature):
         for i in range(numberOfIterations):
+            #taking the first/best solution and inverting 2 indexes to create new neighbour then evaluating it
             neighbour_sol = random_neighbour(sol)
             eval_neighbour_sol = evaluate(neighbour_sol)
-
-            deltaEval = (eval_neighbour_sol - eval_sol)
+            #if the new solution is better, then the new solution becomes the best one
+            deltaEval = eval_neighbour_sol - eval_sol
 
             if (eval_neighbour_sol < eval_sol):
+                #print "eval_neighbour_sol e ", eval_neighbour_sol
                 sol = deepcopy(neighbour_sol)
                 eval_sol = eval_neighbour_sol
             else:
-                if((1 - math.exp(-deltaEval/temperature)) > random.uniform(0,1)):
+                if((math.exp(-deltaEval/temperature)) > random.uniform(0,1)):
                     sol = deepcopy(neighbour_sol)
                     eval_sol = eval_neighbour_sol
 
@@ -213,90 +239,60 @@ def annealing(setOfColours,temperature,numberOfIterations):
     return sol, eval_sol, solListEval
 
 
-#####_______main_____######
+#####_______main_____########################################################
 
 # Get the directory where the file is located
 dir_path = os.path.dirname(os.path.realpath(__file__))
 os.chdir(dir_path) # Change the working directory so we can read the file
-
 
 ncolors, colours = read_file('colours.txt')  # Total number of colours and list of colours
 
 test_size = 100 # Size of the subset of colours for testing
 test_colours = colours[0:test_size]  # list of colours for testing
 
-test_size500 = 500 # Size of the subset of colours for testing
-test_colours500 = colours[0:test_size500]  # list of colours for testing
-
 #permutation = random.sample(range(test_size), test_size) # produces random pemutation of lenght test_size, from the numbers 0 to test_size -1
 #plot_colours(test_colours, permutation)
 
 
-#Greedy algorithm starting with random colour for 100
-'''
-print "------ Greedy algorithm for 100 -----"
+#####Greedy algorithm #####
+
+print "------ Greedy algorithm for ",test_size," sample-----"
 firstRandomColor = random.randint(0,test_size)
 start = time.time()
 greedyColoursList, sumOfGreedySol = constructive(test_colours,firstRandomColor)
 end = time.time()
-print "The time of execution for the greedy algotrithm with 100 colours is: ", end-start, ". The sum of the distances is: ", sumOfGreedySol
-#plot_colours(test_colours, greedyColoursList)
-'''
+print "The time of execution for the greedy algotrithm with ",test_size," colours is: ", end-start, ". The sum of the distances is: ", sumOfGreedySol
+plot_colours(test_colours, greedyColoursList)
 
-#this is to get the range for the sum of distance of Greedy algorithm solution for 100 colours
-'''
-resultCollectorGreedySol100 = []
 
-while(len(resultCollectorGreedySol100)<100):
+#this is to get the range for the sum of distance of Greedy algorithm solution
+'''
+resultCollectorGreedySol = []
+
+while(len(resultCollectorGreedySol)<test_size):
     firstRandomColor = random.randint(0,test_size)
     greedyColoursList, sumOfGreedySol = constructive(test_colours,firstRandomColor)
     resultCollectorGreedySol100.append(sumOfGreedySol)
 
-minSolution = min(resultCollectorGreedySol100)
-maxSolution = max(resultCollectorGreedySol100)
+minSolution = min(resultCollectorGreedySol)
+maxSolution = max(resultCollectorGreedySol)
 
-print "The approximate range of solution for distance with the greedy algorithm for 100 colours is between ", minSolution ," and ",maxSolution
+print "The approximate range of solution for distance with the greedy algorithm for ",test_size," colours is between ", minSolution ," and ",maxSolution
 '''
 
-#Greedy algotrithm starting with random colour for 500
-'''
-print "------ Greedy algorithm for 500 -----"
-firstRandomColor = random.randint(0,test_size500)
-start = time.time()
-greedyColoursList, sumOfGreedySol = constructive(test_colours500,firstRandomColor)
-end = time.time()
-print "The time of execution for the greedy algotrithm with 500 colours is: ", end-start, ". The sum of the distances is: ", sumOfGreedySol
-#plot_colours(test_colours500, greedyColoursList)
-'''
+hillclimbingIterations = 3000
+multiIterations = 30
 
-#this is to get the range for the sum of distance of Greedy algorithm solution for 500 colours
-'''
-resultCollectorGreedySol500 = []
-
-while(len(resultCollectorGreedySol500)<100):
-    firstRandomColor = random.randint(0,test_size500)
-    greedyColoursList, sumOfGreedySol = constructive(test_colours500,firstRandomColor)
-    resultCollectorGreedySol500.append(sumOfGreedySol)
-
-print resultCollectorGreedySol500
-minSolution = min(resultCollectorGreedySol500)
-maxSolution = max(resultCollectorGreedySol500)
-
-print "The approximate range of solution for distance with the greedy algorithm for 500 colours is between ", minSolution ," and ",maxSolution
-'''
-
-hillclimbingIterations = 650
-multiHCIterations = 20
-'''
-#Starting with 100
+#### Hill Climbing algotrithm ####
+print "------ Hill Climbing algorithm for ",test_size," sample-----"
 start = time.time()
 hillclimbingIndexes, sumofHillclimbingSol, valuesHillClimb = hill_climbing(test_colours, hillclimbingIterations)
 end = time.time()
-print "The time of execution for the hill-climbing with 200 iterations for 100 sample list is: ", end - start
+print "The time of execution for the hill-climbing with",hillclimbingIterations,"iterations for ",test_size," sample list is: ", end - start
 print "The sum of distances for hill_climbing is: ", sumofHillclimbingSol
 plot_colours(test_colours, hillclimbingIndexes)
 
-#populating a list from 1 until the the lenght of valuesHillClimb
+#populating a list from 1 until the the lenght of valuesHillClimb to be used in the graph
 improvements = []
 for iter in range(len(valuesHillClimb)):
     improvements.append(iter)
@@ -304,64 +300,53 @@ for iter in range(len(valuesHillClimb)):
 plt.figure()
 plt.plot(improvements,valuesHillClimb)
 plt.ylabel('values')
-plt.xlabel('number of times improved')
-plt.title("Improvements in Hill Climbing algotrithm for 100")
+plt.xlabel('number of times iterated')
+plt.title("Improvements in Hill Climbing algotrithm")
 plt.show()
+
 '''
-'''
-#starting with 500
+##### Multi Hill Climbing #####
+print "------ Multi Hill Climbing algorithm for ", multiIterations ," times with Hill Climbing at ",hillclimbingIterations," iterations -----"
 start = time.time()
-hillclimbingIndexes, sumofHillclimbingSol, valuesHillClimb = hill_climbing(test_colours500, hillclimbingIterations)
-end = time.time()
-print "The time of execution for the hill-climbing with 200 iterations for 500 sample list is: ", end - start
-print "The sum of distances for hill_climbing is: ", sumofHillclimbingSol
-plot_colours(test_colours500, hillclimbingIndexes)
-
-#populating a list from 1 until the the lenght of valuesHillClimb
-improvements = []
-for iter in range(len(valuesHillClimb)):
-    improvements.append(iter)
-
-
-plt.figure()
-plt.plot(improvements,valuesHillClimb)
-plt.ylabel('values')
-plt.xlabel('number of times improved')
-plt.title("Improvements in Hill Climbing algotrithm for 500")
-plt.show()
-'''
-'''
-#return values bestSolIndexes, bestSolEval,  (worstSolIndexes, worstSolEval, are only for the range of solutions)
-#starting with 100
-start = time.time()
-bestSolIndexes, bestSolEval, worstSolIndexes,worstSolEval = multi_hc(multiHCIterations, test_colours, hillclimbingIterations)
-end = time.time()
-
-print "The time of the multi_hc is: ", end - start
-print "The best solution's sum of distances: ", bestSolEval
-print "The worst solution is: ", worstSolEval
-#print "list of values", bestSolIndexes
-#print "list of worst values", worstSolIndexes
-#plot_colours(test_colours, bestSolIndexes)
-
-#starting with 500
-
-start = time.time()
-bestSolIndexes, bestSolEval, worstSolIndexes, worstSolEval = multi_hc(multiHCIterations, test_colours500, hillclimbingIterations)
+bestSolIndexes, bestSolEval, valuesMulti = multi_hc(multiIterations, test_colours, hillclimbingIterations)
 end = time.time()
 print "The time of the multi_hc is: ", end - start
 print "The best solution's sum of distances: ", bestSolEval
-print "The worst solution is: ", worstSolEval
-#print "list of values", bestSolIndexes
-#plot_colours(test_colours500, bestSolIndexes)
-'''
+print "The Standard Deviation is: ", np.std(valuesMulti)
+print "The mean is: ", np.mean(valuesMulti)
+print "The median is: ", np.median(valuesMulti)
+fig4, ax4 = plt.subplots()
+ax4.set_title('Multi Hill Climbing boxplot')
+ax4.boxplot(valuesMulti, showfliers=False)
+plt.show()
+plot_colours(test_colours, bestSolIndexes)
 
-#annealing for 100
+
 temperatureAnnealing = 3
+simulatedAnnealingIterations = 400
+
+
+#### Simulated Annealing ####
+print "------ Simulated Annealing algorithm for ",test_size," sample-----"
 start = time.time()
-annealingIndecies, sumOfAnnealingEval, valuesAnnealing = annealing(test_colours,temperatureAnnealing, hillclimbingIterations)
+annealingIndecies, sumOfAnnealingEval, valuesAnnealing = annealing(test_colours,temperatureAnnealing, simulatedAnnealingIterations)
 end = time.time()
-print "The time of execution for the annealing with 200 iterations and temp of 100 for 100 sample list is: ", end - start
+print "The time of execution for the annealing with ",simulatedAnnealingIterations," iterations and temperature of ",temperatureAnnealing," for ",test_size," sample list is: ", end - start
 print "The sum of distances for simulated_annealing is: ", sumOfAnnealingEval
-print "Values gathered", valuesAnnealing
 plot_colours(test_colours, annealingIndecies)
+
+print "------ Multi Simulated Annealing algorithm for ", multiIterations ," times with SA at ",simulatedAnnealingIterations," iterations -----"
+start = time.time()
+bestSolIndecies, bestSolEval, valuesMulti = multi_sa(multiIterations, test_colours,temperatureAnnealing, simulatedAnnealingIterations)
+end = time.time()
+print "The time of the multi_hc is: ", end - start
+print "The best solution's sum of distances: ", bestSolEval
+print "The Standard Deviation is: ", np.std(valuesMulti)
+print "The mean is: ", np.mean(valuesMulti)
+print "The median is: ", np.median(valuesMulti)
+plot_colours(test_colours, bestSolIndecies)
+fig4, ax4 = plt.subplots()
+ax4.set_title('Multi Simulated annealing boxplot')
+ax4.boxplot(valuesMulti, showfliers=False)
+plt.show()
+'''
